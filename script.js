8e7f942502162d813256dd0c2bf7c55891b068fa
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-03-02 15:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-const student={
-	name:"harshal"
+const student = {
+  name: "harshal",
 };
-Object.prototype.getKeys=function (){
-	return Object.keys(this);
-}
+
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
